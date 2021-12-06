a6b11b24a143e388b1fa05475c39e7b2f88807ee
khoatrn-dev <khoatrn.dev@gmail.com>
2021-12-06 14:38:21 +0700

    Adjusted Text component

--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -2,16 +2,17 @@ import styled from 'styled-components';
 
 const Text = styled.div`
   color: white;
-  width: 20%;
+  max-width: 35%;
   padding: 5px;
+  margin: 5px;
   border: 1px solid white;
   border-radius: 4px;
   background-color: rgba(0, 0, 0, 0.609);
-  height: fit-content;
+  max-height: 50%;
 
   transition: all ease 0.5s;
 
-  opacity: 0;
+  /* opacity: 0; */
 
   display: flex;
   flex-direction: column;
