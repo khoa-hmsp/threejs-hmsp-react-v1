43b8d52189a9bd9d2802235c47fe18ad43858248
khoatrn-dev <khoatrn.dev@gmail.com>
2021-12-06 16:11:34 +0700

    Cleaned up some geometries not used

--- src/pages/ProductDetail/ProductCanvas/threejs/Experience/World/Geometries/ViewPointer/Edges/vertex.glsl
+++
@@ -1,9 +0,0 @@
-uniform vec3 uOffset;
-
-void main()
-{
-  vec3 newPosition = vec3(position.x + uOffset.x, 
-                          position.y + uOffset.y, 
-                          position.z + uOffset.z);
-  gl_Position = vec4(newPosition, 1.0);
-}
