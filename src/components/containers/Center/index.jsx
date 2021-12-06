670facd3e08c3a2ebd991ef0434d34db85da44e0
khoatrn-dev <khoatrn.dev@gmail.com>
2021-12-06 14:51:26 +0700

    Added Center component

---
+++ src/components/containers/Center/index.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { OverlayWebGL } from '..';
+
+const Center = styled(OverlayWebGL)`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  margin: 5px 0 5px 0;
+`;
+
+export default Center;
