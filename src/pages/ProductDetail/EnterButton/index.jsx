d46b970b6c9b97283fdc982e79789a320f7b545c
khoatrn-dev <khoatrn.dev@gmail.com>
2021-12-07 17:26:15 +0700

    Call experience to focus when entering model

--- src/pages/ProductDetail/EnterButton/index.jsx
+++ src/pages/ProductDetail/EnterButton/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../../components/Button';
 import { Container } from '../../../components/containers';
 import Center from '../../../components/containers/Center';
 import { toggleEnterModel } from '../../../redux/experience/slice';
+import Experience from '../ProductCanvas/threejs/Experience/Experience';
 
 export default function EnterButton() {
+  const [experience, setExperience] = useState(null);
+
+  useEffect(() => {
+    setExperience(new Experience());
+  }, []);
+
   const dispatch = useDispatch();
   const isSwitchingModel = useSelector(
     (state) => state.experience.isSwitchModel
@@ -16,6 +23,14 @@ export default function EnterButton() {
   const isEnteringModel = useSelector((state) => state.experience.isEnterModel);
 
   const handleEnterExitModel = () => {
+    if (experience && experience instanceof Experience) {
+      if (!isEnteringModel) {
+        experience.focusCurrentModel();
+      } else {
+        experience.loseFocusCurrentModel();
+      }
+    }
+
     dispatch(toggleEnterModel(!isEnteringModel));
   };
 
