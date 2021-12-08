281511f9d43119e4b8b56cfee18d0b5a53b46dc2
khoatrn-dev <khoatrn.dev@gmail.com>
2021-12-08 11:32:04 +0700

    Added select size field for LeftForm

--- src/pages/ProductDetail/FormSection/LeftForm/index.jsx
+++ src/pages/ProductDetail/FormSection/LeftForm/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Title } from '../../../../components/Typography';
+import { Container, Left } from '../../../../components/containers';
 
 export default function LeftForm() {
   return (
     <form>
-      <Title variant="h1" color="white">
-        Form Left
-      </Title>
-      <input type="text" placeholder="Text input" />
-      <button type="submit">Submit</button>
+      <Container>
+        <Left alignItems="flex-start" flexDirection="column">
+          <Title variant="h1" color="white">
+            Form Left
+          </Title>
+          <label htmlFor="sizeSelect">
+            <Title color="white" variant="h3">
+              Size
+            </Title>
+          </label>
+          <select id="sizeSelect" name="size">
+            <option value="1x1x1">1x1x1</option>
+            <option value="1x2x3">1x2x3</option>
+            <option value="2x1x3">2x1x3</option>
+          </select>
+        </Left>
+      </Container>
     </form>
   );
 }
