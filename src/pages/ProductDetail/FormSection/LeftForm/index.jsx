1b2b6d625741f28550c454a0c6cf2dfbe90f28f9
khoatrn-dev <khoatrn.dev@gmail.com>
2021-12-08 13:44:10 +0700

    Applied react-hook-form LeftForm

--- src/pages/ProductDetail/FormSection/LeftForm/index.jsx
+++ src/pages/ProductDetail/FormSection/LeftForm/index.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Left } from '../../../../components/containers';
 import { Title } from '../../../../components/Typography';
+import { useForm } from 'react-hook-form';
 
 export default function LeftForm() {
+  const { watch, register } = useForm();
+  const watchSize = watch('size', '1x1x1');
+
+  useEffect(() => {
+    console.log(
+      '🚀 ~ file: index.jsx ~ line 13 ~ LeftForm ~ watchSize',
+      watchSize
+    );
+  }, [watchSize]);
+
   return (
     <form>
       <Left alignItems="flex-start" flexDirection="column">
         <Title variant="h1" color="white">
           Form Left
         </Title>
+        {/* Size */}
         <label htmlFor="sizeSelect">
           <Title color="white" variant="h3">
             Size
           </Title>
         </label>
-        <select id="sizeSelect" name="size">
+        <select id="sizeSelect" {...register('size')}>
           <option value="1x1x1">1x1x1</option>
           <option value="1x2x3">1x2x3</option>
           <option value="2x1x3">2x1x3</option>
