3a11a9d14786256a4f50fd2892e2ede1a68a0e85
khoatrn-dev <khoatrn.dev@gmail.com>
2021-12-08 09:14:54 +0700

    Hide elements when entering model

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import EnterButton from './EnterButton';
 import LeftRight from './LeftRight';
 import LoadingContent from './LoadingContent';
 import ProductCanvas from './ProductCanvas';
 
 export default function ProductDetail() {
+  const experienceRedux = useSelector((state) => state.experience);
+
   return (
     <div>
       <ProductCanvas />
-      <LeftRight />
-      <LoadingContent />
+      {!experienceRedux.isEnterModel ? (
+        <>
+          <LeftRight />
+          <LoadingContent />
+        </>
+      ) : null}
       <EnterButton />
     </div>
   );
