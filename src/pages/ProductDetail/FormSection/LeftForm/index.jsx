11a613067cadee85dadcc0958ef9ed07342edd43
khoatrn-dev <khoatrn.dev@gmail.com>
2021-12-08 10:58:25 +0700

    Applied Title component to LeftForm & RightForm

--- src/pages/ProductDetail/FormSection/LeftForm/index.jsx
+++ src/pages/ProductDetail/FormSection/LeftForm/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Title } from '../../../../components/Typography';
 
 export default function LeftForm() {
   return (
     <form>
-      <h1>Form Left</h1>
+      <Title variant="h1" color="white">
+        Form Left
+      </Title>
       <input type="text" placeholder="Text input" />
       <button type="submit">Submit</button>
     </form>
--- src/pages/ProductDetail/FormSection/RightForm/index.jsx
+++ src/pages/ProductDetail/FormSection/RightForm/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Title } from '../../../../components/Typography';
 
 export default function RightForm() {
   return (
     <form>
-      <h1>Form Right</h1>
+      <Title variant="h1" color="white">
+        Form Right
+      </Title>
       <input type="text" placeholder="Text input" />
       <button type="submit">Submit</button>
     </form>
